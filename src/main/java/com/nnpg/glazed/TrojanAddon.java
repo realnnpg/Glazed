f9768d5c02aab7fb0b8d6844a29c3f7ce2889083
IdkGotBoredAsf <pyraclient@gmail.com>
2025-11-03 16:57:33 +0000

    TrojanAddon.java

--- src/main/java/com/nnpg/glazed/TrojanAddon.java
+++ src/main/java/com/nnpg/glazed/TrojanAddon.java
@@ -38,78 +38,35 @@ public static final Category pvp = new Category("TROJAN COMBAT", new ItemStack(I
         Modules.get().add(new AntiTrap()); //done
         Modules.get().add(new CoordSnapper()); //done
         Modules.get().add(new ElytraSwap()); //done
-        Modules.get().add(new PlayerDetection()); //done
         Modules.get().add(new AHSniper()); //done
-        Modules.get().add(new RTPer()); //done
-        Modules.get().add(new ShulkerDropper()); //done
-        Modules.get().add(new AutoSell()); //done
-        Modules.get().add(new SpawnerDropper()); //done
-        Modules.get().add(new AutoShulkerOrder()); // done
-        Modules.get().add(new AutoOrder()); //done
         Modules.get().add(new HideScoreboard());
         Modules.get().add(new LegitCrystalMacro());
-        Modules.get().add(new AHSell());
-        Modules.get().add(new AnchorMacro());
         Modules.get().add(new OneByOneHoles());
         Modules.get().add(new KelpESP());
         Modules.get().add(new RotatedDeepslateESP());
-        Modules.get().add(new CrateBuyer());
-        Modules.get().add(new WanderingESP());
         Modules.get().add(new VillagerESP());
         Modules.get().add(new TpaMacro());
-        Modules.get().add(new TabDetector());
-        Modules.get().add(new OrderSniper());
         Modules.get().add(new PillagerESP());
         Modules.get().add(new HoleTunnelStairsESP());
         Modules.get().add(new CoveredHole());
-        Modules.get().add(new AutoShulkerShellOrder());
-        Modules.get().add(new EmergencySeller());
-        Modules.get().add(new RTPEndBaseFinder());
-        Modules.get().add(new ShopBuyer());
-        Modules.get().add(new OrderDropper());
         Modules.get().add(new VineESP());
         Modules.get().add(new ChunkFinder());
         Modules.get().add(new LegitAnchorMacro());
-        Modules.get().add(new SpawnerOrder());
         Modules.get().add(new RegionMap());
-        Modules.get().add(new SpawnerOrder());
-        Modules.get().add(new RegionMap());
-        Modules.get().add(new SpawnerOrder());
-        Modules.get().add(new RegionMap());
-        Modules.get().add(new AutoShulkerShellOrder());
-        Modules.get().add(new NoBlockInteract());
-        Modules.get().add(new WindPearlMacro());
         Modules.get().add(new QuickShieldBreaker());
-        Modules.get().add(new UDTriggerBot());
-        Modules.get().add(new SwordPlaceObsidian());
-        Modules.get().add(new ChestAndShulkerStealer());
         Modules.get().add(new DoubleAnchorMacro());
         Modules.get().add(new StunMace());
-        Modules.get().add(new HoldHotbarTotem());
-        Modules.get().add(new ElytraFirework());
-        Modules.get().add(new TpaAllMacro());
-        Modules.get().add(new RTPNetherBaseFinder());
-        Modules.get().add(new HomeReset());
         Modules.get().add(new KeyPearl());
         Modules.get().add(new DrownedTridentESP());
         Modules.get().add(new RTPBaseFinder());
         Modules.get().add(new HoverTotem());
-        Modules.get().add(new TapResetMacro());
-        Modules.get().add(new JumpResetMacro());
         Modules.get().add(new TunnelBaseFinder());
-        Modules.get().add(new AimAssist());
-        Modules.get().add(new HumanTriggerBot());
         Modules.get().add(new SkeletonESP());
         Modules.get().add(new RainNoti());
         Modules.get().add(new AutoPearlChain());
-        Modules.get().add(new AutoBlazeRodOrder());
-        Modules.get().add(new SwordAxePlaceCobweb());
-        Modules.get().add(new BlazeRodDropper());
         Modules.get().add(new BreachSwap());
         Modules.get().add(new FakeScoreboard());
-        Modules.get().add(new AutoInvTotem());
         Modules.get().add(new FreecamMining());
-        Modules.get().add(new UIHelper());
 
 
         MeteorClient.EVENT_BUS.subscribe(this);
